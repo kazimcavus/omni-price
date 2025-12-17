695d20ead116f8d23d2a393ea5b7a7581ca65219
Kazım Ç. <129855048+kazimcavus@users.noreply.github.com>
2025-12-18 00:37:55 +0300

    feat: Update project setup for Vite and React
    Integrate Tailwind CSS and update import maps for React and Vite.
    Remove unused CSS import and adjust Vite base path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/omni-price/', // 🔴 ZORUNLU
   plugins: [react()],
 })
