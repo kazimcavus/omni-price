afc7ed9b0249192878131b878f27da7673c4f10a
Kazım Ç. <129855048+kazimcavus@users.noreply.github.com>
2025-12-18 00:25:40 +0300

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/omni-price/', // 🔴 BU OLMAZSA OLMAZ
+  base: '/omni-price/',
   plugins: [react()],
   resolve: {
     alias: {
